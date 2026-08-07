8995285518501522a524f113eef00b2de92869bd
the-vedant-gupta <vedantgupta8784@gmail.com>
2026-08-08 00:39:16 +0530

    sql

--- Questions/Q4.sql
+++ Questions/Q4.sql
@@ -2,7 +2,11 @@
 only customers with a score not equal to 0 
 and return only those countries with an average score gretaer than 430*/
 
-SELECT 
-	id,
-	country
+SELECT	
+	country,
+	AVG(score) AS avg_score
+FROM customers
+WHERE score != 0 
+GROUP BY country
+HAVING AVG(score) > 430 
 
